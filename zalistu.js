850a68d18c4d009a69ed5a25112b62b3f8938189
TeaMarusic <tea.marusic@fsre.sum.ba>
2024-05-23 18:44:07 +0200

    dodavanje nekih funkcija

--- zalistu.js
+++ zalistu.js
@@ -7,3 +7,6 @@ for (let i=0;i<niz.length;i++){
         x.append(novi);;
     }
 }
+function funkcija(){
+window.open('https://www.google.com/search?q=dog+pcture&rlz=1C1SQJL_hrBA893BA893&oq=dog+pcture&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIJCAEQABgNGIAEMgkIAhAAGA0YgAQyCQgDEAAYDRiABDIJCAQQABgNGIAEMgkIBRAAGA0YgAQyCQgGEAAYDRiABDIJCAcQABgNGIAEMgkICBAAGA0YgAQyCQgJEAAYDRiABNIBCDIxMjBqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8#vhid=rLyV376IoQ8n5M&vssid=l','_blank');
+}
